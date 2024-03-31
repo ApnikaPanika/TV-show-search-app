65faf5f295f436f738a123812c5eb385096e9382
ApnikaPanika <budetoms2000@gmail.com>
2024-03-31 23:15:27 +0300

    Solved lint errors regarding newest react version

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+} else {
+  throw new Error('Root element not found in the document.');
+}
 
 reportWebVitals();
